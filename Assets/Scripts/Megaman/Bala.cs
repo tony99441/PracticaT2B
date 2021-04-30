fc8a0bd3098fcefd3eed760fe7e26f581dc523ae
Anthony Briones Quiroz <n00017665@upn.pe>
2021-04-30 09:02:42 -0500

    Practicat2-b

---
+++ Assets/Scripts/Megaman/Bala.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    private Rigidbody2D BalaRb;
+    public float VelocidadBala;
+    public float vidaBala;
+
+    private GameObject player;
+    private SpriteRenderer playerSr;
+    
+
+    void Awake()
+    {
+        BalaRb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+        playerSr =player.GetComponent<SpriteRenderer>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!playerSr.flipX)
+        {
+            BalaRb.velocity=new Vector2(VelocidadBala,BalaRb.velocity.y);
+        }
+        else
+        {
+            BalaRb.velocity=new Vector2(-VelocidadBala,BalaRb.velocity.y);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject,vidaBala);
+    }
+}
---
+++ Assets/Scripts/Megaman/MegamanPlayer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegamanPlayer : MonoBehaviour
+{
+    
+    private Rigidbody2D MegamanRb;
+    private SpriteRenderer MegamanSr;
+    private Animator MegamanAnimator;
+    
+    public bool grounded;
+
+    private bool DobleJumped;
+    
+
+    public float FuerzaSalto ;
+
+    public Transform balaPosition;
+
+    public GameObject bala;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MegamanRb = GetComponent<Rigidbody2D>();
+        MegamanSr = GetComponent<SpriteRenderer>();
+        MegamanAnimator = GetComponent<Animator>();
+        
+    }
+
+ 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grounded)
+            DobleJumped = false;
+       
+        PlayerMovement();
+        PlayerJump();
+        PlayerDisparo();
+    }
+    public void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MegamanSr.flipX = false;
+            MegamanRb.velocity = new Vector2(10, MegamanRb.velocity.y);
+           
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MegamanSr.flipX = true;
+            MegamanRb.velocity = new Vector2(-10, MegamanRb.velocity.y);
+           
+        }
+        MegamanAnimator.SetFloat("estado",Mathf.Abs(MegamanRb.velocity.x));
+        
+    }
+
+    public void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            MegamanRb.velocity = new Vector2(MegamanRb.velocity.x,FuerzaSalto);
+        }
+       
+        MegamanAnimator.SetFloat("Salto",Mathf.Abs(MegamanRb.velocity.y));
+        // Cuando presione la tecla derecha cambia la animación.
+        // Input.GetKey() // mientras mantenga presionado
+        // Input.GetKeyUp() // Cuando suelto la tecla
+        // Input.GetKeyDown // Cuando presiono por primera vez
+    }
+
+    public void PlayerDisparo()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bala, balaPosition.position, balaPosition.rotation);
+        }
+    }
+    
+}
